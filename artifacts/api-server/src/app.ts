b3150ddeaf60db4e64cf31c4bba95bce861b8a9c
aizdhuka-maker <aizdhuka@gmail.com>
2026-06-02 20:55:14 +0000

    fix explicit imports

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,8 +1,8 @@
 import express, { type Express } from "express";
 import cors from "cors";
 import { pinoHttp }  from "pino-http";
-import router from "./routes/index";
-import { logger } from "./lib/logger/index";
+import router from "./routes/index.js";
+import { logger } from "./lib/logger/index.js";
 
 const app: Express = express();
 
